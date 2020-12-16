5a6890f070d7f53ebe707bde0e28c90ed0b95793
Sumanth Nagadavalli <sumanth.nagadavalli@myob.com>
2020-12-16 11:29:00 +1100

    First commit - merge sort

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # algotihms-typescript
+
 Implementation of famous algorithms in typescript
+
+## How to run?
+
+### Prerequisites
+
+> [node](https://nodejs.org/en/)
+
+> [yarn](https://yarnpkg.com/)
+
+### Install dependencies
+
+> yarn
+
+### Run test
+
+> yarn test
---
+++ src/mergeSort.ts
@@ -0,0 +1,42 @@
+const mergeSort = (incomingArray: number[]): number[] => {
+  const arrLength = incomingArray.length;
+  // base condition for recursion
+  if (arrLength <= 1) {
+    return incomingArray;
+  }
+
+  // divide incoming array into left and right on midpoint
+  const midLength = arrLength % 2 === 0 ? arrLength / 2 : (arrLength + 1) / 2;
+  const leftArray = incomingArray.slice(0, midLength);
+  const rightArray = incomingArray.slice(midLength, arrLength);
+
+  // sort left and right array
+  const sortedLeftArray = mergeSort(leftArray);
+  const sortedRightArray = mergeSort(rightArray);
+
+  // merge left and right array
+  var mergedArray = [];
+  let leftArrayPointer = 0;
+  let rightArrayPointer = 0;
+
+  for (var k = 0; k < arrLength; k++) {
+    if (typeof sortedLeftArray[leftArrayPointer] === "undefined") {
+      mergedArray[k] = sortedRightArray[rightArrayPointer];
+      rightArrayPointer++;
+    } else if (typeof sortedRightArray[rightArrayPointer] === "undefined") {
+      mergedArray[k] = sortedLeftArray[leftArrayPointer];
+      leftArrayPointer++;
+    } else if (
+      sortedLeftArray[leftArrayPointer] < sortedRightArray[rightArrayPointer]
+    ) {
+      mergedArray[k] = sortedLeftArray[leftArrayPointer];
+      leftArrayPointer++;
+    } else {
+      mergedArray[k] = sortedRightArray[rightArrayPointer];
+      rightArrayPointer++;
+    }
+  }
+  return mergedArray;
+};
+
+export default mergeSort;
