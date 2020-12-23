ec8697d9b2a4bf651e9d178022c355f6108e2712
sumanthns-commits <nsready@gmail.com>
2020-12-23 18:51:33 +1100

    Test

--- src/__tests__/sortPoints.test.ts
+++ src/__tests__/sortPoints.test.ts
@@ -27,4 +27,30 @@ describe("sort points", () => {
 
     expect(actualSortedPointsByX).toEqual(expectedSortedPointsByX);
   });
+
+  it("should be able to sort an array of points by their y co-ordinate", () => {
+    const points = [
+      new Point(5, 6),
+      new Point(-1, 5),
+      new Point(0, 0),
+      new Point(100, 1),
+      new Point(9, -4),
+    ];
+
+    const expectedSortedPointsByY = [
+      new Point(9, -4),
+      new Point(0, 0),
+      new Point(100, 1),
+      new Point(-1, 5),
+      new Point(5, 6),
+    ];
+
+    const actualSortedPointsByY = mergeSort(
+      points,
+      (point1: Point, point2: Point) =>
+        point1.y < point2.y ? -1 : point1.y > point2.y ? 1 : 0
+    );
+
+    expect(actualSortedPointsByY).toEqual(expectedSortedPointsByY);
+  });
 });
